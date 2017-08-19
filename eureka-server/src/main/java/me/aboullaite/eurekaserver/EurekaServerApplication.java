358a039c0f78d98de2d5bd375efb6b92ff6efb4a
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2017-08-19 20:48:45 +0100

    Fix typos

--- eureka-server/src/main/java/me/aboullaite/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/me/aboullaite/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package me.aboullaite.eurikaserver;
+package me.aboullaite.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurikaServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurikaServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
